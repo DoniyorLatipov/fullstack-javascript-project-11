c3d884366d1dc83e7bb03e62bf847a3077a13a52
Doniyor Latipov <doniyor.main.contact@gmail.com>
2025-05-11 17:24:25 +0500

    feat: add handling and rendering for feeds and posts display

--- src/view.js
+++ src/view.js
@@ -1,6 +1,6 @@
 import onChange from 'on-change';
 
-const setWatcher = (state, i18n, { input, output }) =>
+const setWatcher = (state, i18n, { input, output, postsContainer, feedsContainer }) =>
   onChange(state, (path, value) => {
     // for input ui
     if (path === 'ui.input.state') {
@@ -23,8 +23,30 @@ const setWatcher = (state, i18n, { input, output }) =>
     }
 
     //for adding links
-    if (path === 'parsedLinks') {
-      // render parsedLinks
+    if (path === 'feeds') {
+      // render feeds
+      const feeds = value.map((feed) => {
+        const feedElement = document.createElement('li');
+        feedElement.classList.add('list-group-item', 'border-0', 'border-end-0');
+        feedElement.innerHTML = `<h3>${feed.name}</h3><p>${feed.description}</p>`;
+        return feedElement;
+      });
+
+      feedsContainer.innerHTML = '';
+      feedsContainer.append(...feeds);
+    }
+
+    if (path === 'posts') {
+      // render posts
+      const posts = value.map((post) => {
+        const postElement = document.createElement('li');
+        postElement.classList.add('list-group-item', 'border-0', 'border-end-0');
+        postElement.innerHTML = `<a href="${post.link}" target="_blank" rel="noopener noreferrer">${post.title}</a>`;
+        return postElement;
+      });
+
+      postsContainer.innerHTML = '';
+      postsContainer.append(...posts);
     }
 
     if (path === 'ui.input.error') {

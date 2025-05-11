8264a22474c131c68ed8f33952d61e57178711d4
Doniyor Latipov <doniyor.main.contact@gmail.com>
2025-05-11 19:28:27 +0500

    feat: add RSS post updating every 5s using setTimeout to avoid request collision

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import setWatcher from './view.js';
+import IdGenerator from './idGenerator.js';
 import { validateUrl } from './validation.js';
 import rssParser from './rssParser.js';
 
@@ -22,6 +23,41 @@ export default async (i18n) => {
     feedsContainer: document.querySelector('.feeds'),
   };
 
+  let timerId = null;
+
+  function updateRss() {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      const { feeds } = watchedState;
+      const promises = feeds.map(({ link, id }) => rssParser(link, id));
+      Promise.allSettled(promises)
+        .then((results) => {
+          const newPosts = results
+            .flatMap((result) => {
+              console.log(result);
+
+              if (result.status !== 'fulfilled') return null;
+              const { posts } = result.value;
+              return posts;
+            })
+            .filter(Boolean)
+            .filter((post) => {
+              const isPostExists = watchedState.posts.some((p) => p.link === post.link);
+              return !isPostExists;
+            });
+
+          watchedState.posts.push(...newPosts);
+        })
+        .then(() => {
+          console.log('tested');
+
+          updateRss;
+        });
+    }, 5000);
+  }
+
+  const idGen = new IdGenerator();
   const watchedState = setWatcher(state, i18n, elements);
 
   elements.form.addEventListener('submit', (e) => {
@@ -44,6 +80,9 @@ export default async (i18n) => {
         // adding link
         watchedState.feeds.push({ title, description, link, id });
         watchedState.posts.push(...posts);
+      })
+      .then(() => {
+        updateRss();
       })
       .catch((err) => {
         const key = err.isAxiosError ? 'network' : err.message.key;
